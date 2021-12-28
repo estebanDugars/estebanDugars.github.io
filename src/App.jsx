3d82aec32a678531e021ae11802efb978d7f5180
mig <test@gmail.com>
2021-12-28 18:01:02 +0100

    Styles margin align-itels: flex-end

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
 import "./App.css";
-import HomePage from './Pages/HomePage';
-import Articles from './Pages/Articles';
-import Article from './Pages/Article';
-import About from './Pages/About';
-import CMS from './Pages/CMS';
-import Navbar from './Navbar';
-import { Routes,Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import Articles from "./Pages/Articles";
+import Article from "./Pages/Article";
+import About from "./Pages/About";
+import CMS from "./Pages/CMS";
+import Navbar from "./Navbar";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p className="App-logo">paragraph inside header</p>
-        <Navbar/>
+        <Navbar />
       </header>
       <Routes>
-      <Route path='/' exact element={<HomePage />} />
-      <Route path='/Articles' element={ <Articles />} />
-      <Route path='/Article' element={<Article />} />
-      <Route path='/About' element={<About />} />
-        <Route path='/CMS' element={<CMS />} />
+        <Route path="/" exact element={<HomePage />} />
+        <Route path="/Articles" element={<Articles />} />
+        <Route path="/Article" element={<Article />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/CMS" element={<CMS />} />
       </Routes>
     </div>
-    );
+  );
 }
 
 export default App;
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
-    return (
-        <ul className='navbar'>
-            <Link to="/" className='nav-item'>Home</Link>
-            <Link to="/Articles" className='nav-item'>Articles</Link>
-            <Link to="/Article" className='nav-item'>Article</Link>
-            <Link to="/About" className='nav-item'>About</Link>
-            <Link to="/CMS" className='nav-item'>CMS</Link>
-        </ul>
-    );
+  return (
+    <ul className="navbar">
+      <Link to="/">Home</Link>
+      <Link to="/Articles">Articles</Link>
+      <Link to="/Article">Article</Link>
+      <Link to="/About">About</Link>
+      <Link to="/CMS">CMS</Link>
+    </ul>
+  );
 };
 
 export default Navbar;
