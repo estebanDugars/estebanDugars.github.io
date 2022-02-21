c3b6b31fccc6bb9f6aea77e6a4cf2bdd4862ca78
mig <test@gmail.com>
2022-02-21 10:40:33 +0100

    cv

---
+++ public/cv/assets/js/freelancer.js
@@ -0,0 +1,81 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 70)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+    $('.scroll-to-top').css({visibility:"hidden"});
+    
+  // Scroll to top button appear
+  $(document).scroll(function() {
+    var scrollDistance = $(this).scrollTop();
+    if (scrollDistance > 100) {
+      //$('.scroll-to-top').fadeIn();
+        $('.scroll-to-top').css({visibility:"visible"});
+    } else {
+      //$('.scroll-to-top').fadeOut();
+        $('.scroll-to-top').css({visibility:"hidden"});
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 80
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+  // Floating label headings for the contact form
+  $(function() {
+    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+      $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+    }).on("focus", ".floating-label-form-group", function() {
+      $(this).addClass("floating-label-form-group-with-focus");
+    }).on("blur", ".floating-label-form-group", function() {
+      $(this).removeClass("floating-label-form-group-with-focus");
+    });
+  });
+    
+  /*  var temp = true;
+  
+  $('img.img-fluid.d-block.mx-auto.mb-5').hover(function(){
+	  
+	  
+	  if (temp)
+	  {this.src='assets/img/yoda.png';
+		temp = false;}
+	  else{
+		this.src='assets/img/profil.png';
+		temp = true;
+	  }
+  })*/
+
+})(jQuery); // End of use strict
